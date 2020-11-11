e489d5276dee7c63e6131ca5cfee33d67d5f5898
jrfer <jrfer@192.168.2.7>
2020-11-11 20:06:11 +0100

    HatMan - room displayed

--- public_html/main.js
+++ public_html/main.js
@@ -1,4 +1,15 @@
 const canvas = document.getElementById('my_canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 930;
+canvas.height = 462;
+
+const background = new Image();
+background.src = "images/firstroom.png";
+
+function animate()
+{
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+    requestAnimationFrame(animate);
+}
+
+animate();
