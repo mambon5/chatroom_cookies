30752bfbfcfd1d6674a553126ac3158d8f32631a
MiPc <MiPc@192.168.1.173>
2020-11-12 00:56:29 +0100

    Merge origin/master
    Conflicts:
    	public_html/main.js

--- public_html/main.js
+++ public_html/main.js
@@ -1,3 +1,6 @@
+let aux1 =  document.getElementById('aux');
+let aux2 =  document.getElementById('aux2');
+
 const canvas = document.getElementById('my_canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 930;
@@ -14,7 +17,7 @@ const player = {
     frameY: 0,
     frameW: 32,
     frameH: 48,
-    speed: 4,
+    speed: 10,
     moving: false,
     hi: function() {return player.x++;}
 };
@@ -25,25 +28,9 @@ persimg.src = "images/henryjones.png";
 const background = new Image();
 background.src = "images/firstroom.png";
 
-let iter = 0;
-function animate()
-{
-    ctx.clearRect(0,0,canvas.width,canvas.height); 
-    ctx.drawImage(background,0,0,canvas.width,canvas.height);    
-    
 
-    
-    ctx.drawImage(persimg, player.frameX*player.frameW, player.frameY*player.frameH, player.width,player.height,player.x,player.y,player.frameW,player.frameH);
 
-    movePlayer();
-    camina();
-    
-    iter++;
-    requestAnimationFrame(animate);
-    
-}
 
-animate();
 
 window.addEventListener("keydown", function(e){
     keys[e.keyCode] = true;
@@ -90,8 +77,51 @@ function movePlayer(){
 }
 
 function camina() {
-    if( (iter % 10) === 0) {
+   
         if(player.frameX < 3 && player.moving) player.frameX++;
         else player.frameX = 0;
-    }
+    
+   
 }
+
+let fpsint, now, then, elapsed;
+
+let iter = 0;
+
+function animate()
+{
+    now = Date.now();
+    elapsed = now - then;
+    aux2.innerHTML = "pos x: " + player.x + " <br>pos y: " + player.y;
+    aux1.innerHTML = "now: " + now + "<br> then: " + then;
+    if(elapsed > fpsint) {
+        
+        then = now;
+    ctx.clearRect(0,0,canvas.width,canvas.height); 
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);    
+    
+
+    
+    ctx.drawImage(persimg, player.frameX*player.frameW, player.frameY*player.frameH, player.width,player.height,player.x,player.y,player.frameW,player.frameH);
+    
+    camina();
+    movePlayer();
+    
+    }
+    ++iter;
+    
+    requestAnimationFrame(animate);
+    
+    
+}
+
+
+
+function startAnime(interval) {
+    fpsint = interval;
+    then = Date.now();
+    aux1.innerHTML = "now: " + now + ", then: " + then;
+    animate();
+}
+
+startAnime(60);
