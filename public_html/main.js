8c73c1156c482dd46c5a32c02e8354a64555ddfd
MiPc <MiPc@192.168.1.173>
2020-11-11 20:51:52 +0100

    Merge origin/master
    Conflicts:
    	public_html/main.html

--- public_html/main.js
+++ public_html/main.js
@@ -9,12 +9,16 @@ persimg.src = "images/henryjones.png";
 const background = new Image();
 background.src = "images/firstroom.png";
 
+
 function animate()
 {
     ctx.drawImage(background,0,0,canvas.width,canvas.height);    
     
-    ctx.drawImage(persimg,0,0,128/4,192/4,10,10,128/4,192/4);
+    ctx.drawImage(persimg, 0, 0, 128/4, 192/4, 10, 10, 128/4, 192/4);
     requestAnimationFrame(animate);
+    
 }
 
 animate();
+
+window.addEventListener("keydown")
