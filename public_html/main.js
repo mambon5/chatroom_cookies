eeb2afc728070c85b741aea20a92bef19c406e6d
MiPc <MiPc@192.168.1.173>
2020-11-11 20:14:48 +0100

    Merge origin/master
    Conflicts:
    	public_html/main.html

--- public_html/main.js
+++ public_html/main.js
@@ -3,12 +3,22 @@ const ctx = canvas.getContext('2d');
 canvas.width = 930;
 canvas.height = 462;
 
+const person = document.getElementById('personatge');
+const personctx = person.getContext('2d');
+person.width = 30;
+person.height = 20;
+
+const persimg = new Image();
+persimg.src = "images/henryjones.png";
+
 const background = new Image();
 background.src = "images/firstroom.png";
 
 function animate()
 {
-    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);    
+    
+    personctx.drawImage(persimg,0,0,person.width,person.height);
     requestAnimationFrame(animate);
 }
 
