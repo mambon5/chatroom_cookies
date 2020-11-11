658bd9ec18a204f44827d1d5886b78cab1d538f2
jrfer <jrfer@192.168.2.7>
2020-11-11 20:51:23 +0100

    HatMan - player displayed, player class added

--- public_html/main.js
+++ public_html/main.js
@@ -3,6 +3,19 @@ const ctx = canvas.getContext('2d');
 canvas.width = 930;
 canvas.height = 462;
 
+const player = {
+    x: 0,
+    y: 0,
+    width: 32,
+    height: 48,
+    frameX: 0,
+    frameY: 0,
+    frameW: 32,
+    frameH: 48,
+    speed: 4,
+    moving: false
+};
+
 const persimg = new Image();
 persimg.src = "images/henryjones.png";
 
@@ -13,7 +26,7 @@ function animate()
 {
     ctx.drawImage(background,0,0,canvas.width,canvas.height);    
     
-    ctx.drawImage(persimg,0,0,128/4,192/4,10,10,128/4,192/4);
+    ctx.drawImage(persimg,player.frameX,player.frameY,player.width,player.height,player.x,player.y,player.frameW,player.frameH);
     requestAnimationFrame(animate);
 }
 
