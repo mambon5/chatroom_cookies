9c5a9cf96f4fdefa58d414f74bddab254c617b72
MiPc <MiPc@192.168.1.173>
2020-11-22 18:29:23 +0100

    Merge origin/master
    Conflicts:
    	public_html/main.js

--- public_html/main.js
+++ public_html/main.js
@@ -12,32 +12,32 @@ const scale = 1.6;
 const map = new Cmap(3,5);
 
 const player = new Cplayer(canvas.width/2, canvas.height/2, 32, 48,
-scale, 10, margins = marg_henry, name="hjones");
+scale, 8, margins = marg_henry, name="hjones");
 player.image.src = "images/henryjones.png"; // 24.25, 45.5
 var units = [player];
 
 const monst1 = new Cmonster(canvas.width/2, canvas.height/2, function() {return this.x - player.x + player.x_init;}, function() {return this.y - player.y + player.y_init;}, 
-                32, 48, scale, 10, margins = marg_cpmerica, name="monst1");
+                32, 48, scale, 8, margins = marg_cpmerica, name="monst1");
 monst1.image.src = "images/captainamerica_shield.png";
 units.push(monst1);
 
 const monst2 = new Cmonster(canvas.width/2, canvas.height/2, function() {return this.x - player.x + player.x_init;}, function() {return this.y - player.y + player.y_init;}, 
-32, 32, scale, 5, margins = marg_greebo2, name="monst2");
+32, 32, scale, 4, margins = marg_greebo2, name="monst2");
 monst2.image.src = "images/greebo2.png";
 units.push(monst2);
 
 const monst3 = new Cmonster(canvas.width/2, canvas.height/2, function() {return this.x - player.x + player.x_init;}, function() {return this.y - player.y + player.y_init;}, 
-32, 48, scale, 5, margins = marg_prodroid2, name="monst3");
+32, 48, scale, 4, margins = marg_prodroid2, name="monst3");
 monst3.image.src = "images/protocoldroid2.png";
 units.push(monst3);
 
 const monst4 = new Cmonster(canvas.width/2, canvas.height/2, function() {return this.x - player.x + player.x_init;}, function() {return this.y - player.y + player.y_init;},
-32, 48, scale, 10, margins = marg_tiana2, name="monst4");
+32, 48, scale, 8, margins = marg_tiana2, name="monst4");
 monst4.image.src = "images/tiana2.png";
 units.push(monst4);
 
 const monst5 = new Cmonster(canvas.width/2, canvas.height/2, function() {return this.x - player.x + player.x_init;}, function() {return this.y - player.y + player.y_init;},
-32, 48, scale, 13, margins = marg_rhodey, name="monst5");
+32, 48, scale, 10, margins = marg_rhodey, name="monst5");
 monst5.image.src = "images/rhodey.png";
 units.push(monst5);
 
